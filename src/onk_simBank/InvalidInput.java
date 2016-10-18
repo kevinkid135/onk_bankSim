0120b3044054a61188de8942f811e4fa83b3f487
Niki Lin <1nl6@queensu.ca>
2016-10-18 01:18:25 -0400

    Created InvalidInput class

---
+++ src/onk_simBank/InvalidInput.java
@@ -0,0 +1,14 @@
+package onk_simBank;
+
+public class InvalidInput extends Exception{
+	
+	/**
+	 * Constructs an exception with a message
+	 * @param message A specified message.
+	 */
+
+	public InvalidInput(String message){
+		super (message);
+	}// end InvalidInput exception
+	
+}// end InvalidInput
