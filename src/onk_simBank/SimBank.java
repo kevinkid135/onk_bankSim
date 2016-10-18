e934fb1083a139c457ecefcc5d420658a795d822
Niki Lin <1nl6@queensu.ca>
2016-10-18 12:57:54 -0400

    Finished transaction. Added notes

--- src/onk_simBank/SimBank.java
+++ src/onk_simBank/SimBank.java
@@ -1,5 +1,8 @@
 package onk_simBank;
 
+// Add search method for account numbers?
+// How can we prevent the creation of multiple accounts with the same account number?
+
 import static java.lang.System.out;
 
 import java.util.ArrayList;
@@ -53,24 +56,42 @@ public class SimBank {
 	 * Method that runs method according to user input
 	 */
 	private void transaction() {
-		out.println("logout? Transaction?");
-		input = in.nextLine();
-		switch (input) {
-		case "create":
-			transactionCreate();
-		case "delete":
-			transactionDelete();
-		case "deposit":
-			transactionDeposit();
-		case "logout":
-			transactionLogout();
-		case "transfer":
-			transactionTransfer();
-		case "withdraw":
-			transactionWithdraw();
-		}
-
-	}
+		// While loop used for repeated prompt after invalid command.
+		boolean command = false;
+		while(!command){
+			out.println("logout? Transaction?");
+			input = in.nextLine();
+			switch(input){
+			case "create":
+				transactionCreate();
+				command = true;
+				break;
+			case "delete":
+				transactionDelete();
+				command = true;
+				break;
+			case "deposit":
+				transactionDeposit();
+				command = true;
+				break;
+			case "logout":
+				transactionLogout();
+				command = true;
+				break;
+			case "transfer":
+				transactionTransfer();
+				command = true;
+				break;
+			case "withdraw":
+				transactionWithdraw();
+				command = true;
+				break;
+				// None of the options above inputed
+			default:
+				System.out.println("Invalid command");
+			}// Close switch statement
+		}//end while-loop
+	}// End transaction method
 
 	/**
 	 * Reads in user input for an account number and checks if it exists in accList.
@@ -78,15 +99,20 @@ public class SimBank {
 	 * Throws an InvalidInput exception if it already exists.
 	 */
 	private String transactionCreate() {
-		// get user input
-		
-		// check if it's valid (here or in account?)
-		
-		// check if it already exists (here or in account?)
-		
-		// throw InvalidInput exception if it doesn't exist
-		
-		return "TT AAA BBB CCCC"; // TO-DO
+		if(sessionType == 2){
+			// get user input
+			out.println("Account number?");
+			// check if it's valid (8 digits, does not start with 0) - throw InvalidInput if not valid
+
+			// check if it already exists - throw Invalid Input if it exists
+
+			// If valid and unique, create transaction message (Add to account list? How to prevent transaction?)
+
+			return "TT AAA BBB CCCC"; // TO-DO
+		}else{
+			System.out.println("Invalid command.");
+			return null;
+		}
 	}
 
 	/**
@@ -103,6 +129,13 @@ public class SimBank {
 	 * @return
 	 */
 	private String transactionDeposit() {
+		int accNum;
+		int amount;
+		out.println("Account number?");
+		input = in.nextLine();
+		// Check arrayList for matching account number - implement a search function?
+		// If match found, use Account to try deposit - success deposit return transaction message
+		// If match not found, print error and return to transaction()
 		return "TT AAA BBB CCCC"; // TO-DO
 	}
 
