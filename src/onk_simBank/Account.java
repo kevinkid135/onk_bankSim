1a07204ef8cce72c54b3825098c4f7b6e7140641
Niki Lin <1nl6@queensu.ca>
2016-10-20 12:54:05 -0400

    Update Account.java

--- src/onk_simBank/Account.java
+++ src/onk_simBank/Account.java
@@ -1,8 +1,7 @@
 package onk_simBank;
 
 /**
- * A class to keep track of the total withdrawn and deposited amounts for each
- * account.
+ * A class to keep track of the total withdrawn amounts for each account.
  * 
  * @author Team Onk
  *
@@ -12,20 +11,40 @@ public class Account {
 	private int withdrawTotal;
 
 	/**
-	 * Constructor for account. Sets the withdrawnTotal and depositTotal to 0
-	 * when account object is created
+	 * Constructor for account. Sets the withdrawnTotal to 0 when account object
+	 * is created.
 	 * 
 	 * @param acc
+	 *            is the account number of the newly created account.
 	 */
 	public Account(int acc) {
 		accNum = acc;
 		withdrawTotal = 0;
 	}// end Account constructor
 
+	/**
+	 * Returns the account number of the current account.
+	 * 
+	 * @return account number
+	 */
 	public int getAccNum() {
 		return accNum;
 	}
 
+	/**
+	 * Checks if the withdraw amount for the current account is within the
+	 * constraints. In atm mode, only withdrawals of 1000 and under are
+	 * accepted. In agent mode, only withdrawals of 99999999 and under are
+	 * accepted. There is a total withdrawal limit of 1000 per account in atm
+	 * mode, and no limit in agent mode.
+	 * 
+	 * @param num
+	 *            is the amount being withdrawn from the current account
+	 * @return true if the withdrawn amount is within the constraints, and false
+	 *         otherwise.
+	 * @throws InvalidInput
+	 *             if the withdrawn amount is not within the constraints.
+	 */
 	public boolean withdraw(int num) throws InvalidInput {
 		int sessionType = SimBank.getSessionType();
 		// check for valid withdraw amount
@@ -51,9 +70,22 @@ public class Account {
 				withdrawTotal += num;
 				return true;
 			}
-		}// end if-else for sessionTypes
+		} // end if-else for sessionTypes
 	}// end updateWithdrawTotal
 
+	/**
+	 * Checks if the deposit amount for the current account is within the
+	 * constraints. In atm mode, only withdrawals of 1000 and under are
+	 * accepted. In agent mode, only withdrawals of 99999999 and under are
+	 * accepted.
+	 * 
+	 * @param num
+	 *            is the amount being withdrawn from the current account
+	 * @return true if the withdrawn amount is within the constraints, and false
+	 *         otherwise.
+	 * @throws InvalidInput
+	 *            if the withdrawn amount is not within the constraints.
+	 */
 	public boolean deposit(int num) throws InvalidInput {
 		int sessionType = SimBank.getSessionType();
 		// check for valid deposit amount
@@ -71,7 +103,7 @@ public class Account {
 			else if (num < 0)
 				throw new InvalidInput("Invalid amount.");
 			return true;
-		}// end if-else for sessionTypes
+		} // end if-else for sessionTypes
 
 	}// end deposit
 
