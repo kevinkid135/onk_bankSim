2772044df612570604d2d42a26e95fc84b0ce011
kevinkid135 <kevinkid228@hotmail.com>
2016-11-06 22:04:21 -0500

    Created skeleton methods

--- src/backEnd/BackEnd.java
+++ src/backEnd/BackEnd.java
@@ -1,10 +1,57 @@
 package backEnd;
 
+import java.util.ArrayList;
+
 public class BackEnd {
+	private static String transSumFilename;
+	private static String masterAccListFilename;
+	private static ArrayList<String> localMasterAccList = new ArrayList<String>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		transSumFilename = args[0];
+		masterAccListFilename = args[1];
+		
+		// parse tranSum into string of arrays
+		// parse master acc list into arraylist
+		
+		// loop through tranSum array
+		for(;;){
+			// if transaction code is deposit
+			// if withdraw
+			// if transfer
+			// if create
+			// if delete
+			// else throw error
+		}
+	}
+
+	public static void create(int accNum, String accName) {
 
 	}
 
+	public static void delete(int accNum, String accName) {
+
+	}
+
+	public static void withdraw(int amount, int accNum) {
+
+	}
+	
+	public static void deposit(int amount, String accNum) {
+
+	}
+	
+	public static void transfer(int accNum1, int accNum2, int amount) {
+
+	}
+	
+	public static void accountExists(int accNum) {
+
+	}
+	
+	public static void findAccount(int accNum) {
+
+	}
+	
+	
 }
